423f609cd04576c197578aafa635e8902b3c012e
Emlice <xiaomozyw@sina.com>
2018-07-17 18:11:20 +0800

    update version

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -1,14 +1,15 @@
 //logs.js
-var util = require('../../utils/util.js')
+const util = require('../../utils/util.js')
+
 Page({
-  data: {
-    logs: []
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(function (log) {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
+    data: {
+        logs: []
+    },
+    onLoad: function() {
+        this.setData({
+            logs: (wx.getStorageSync('logs') || []).map(log => {
+                return util.formatTime(new Date(log))
+            })
+        })
+    }
 })
